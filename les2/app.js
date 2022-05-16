5f84c4b7751e0e24e5361b29b2a3dc698025d280
EdsonHalubets3 <9017154@student.zadkine.nl>
2022-05-16 15:02:21 +0200

    op2

--- les2/app.js
+++ les2/app.js
@@ -14,17 +14,19 @@ let p = document.getElementById("output") ;
 //     country= event.target.value;
 //     changeInput();
 // };
-const changeInput = function(event){
-    const text = `Je heet ${name} en je komt uit ${country}`;
-    p.textContent = text;
-};
-
-let button = document.querySelector("button");
-button.addEventListener("click",function(){
-console.log("geclicktt");
-});
 let nameChanged = document.querySelector("#nameChanged");
+let countryChanged = document.querySelector("#countryChanged");
 nameChanged.addEventListener("change", function(){
     name= event.target.value;
    changeInput()
 });
+countryChanged.addEventListener("change", function(){
+    country= event.target.value;
+   changeInput()
+});
+
+const changeInput = function(){
+    const text = `Je heet ${name} en je komt uit ${country}`; 
+    p.textContent = text;
+}
+

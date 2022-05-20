01040188e76715ebe8369c4e05212848f9310d3e
EdsonHalubets3 <9017154@student.zadkine.nl>
2022-05-20 12:09:08 +0200

    arrow

--- les2/app.js
+++ les2/app.js
@@ -1,7 +1,35 @@
 //Oefening angry-birds
 const bird = document.querySelector(".bird");
 let count = 0;
+
 bird.addEventListener("click", function(){
     count= count + 50;
     bird.style.left= count + "px" ;
 });
+window.addEventListener("keydown",function(e){
+    console.log(e);
+    if(e.key=="ArrowDown"){
+        count= count + 50;
+        bird.style.left= count + "px" ;
+    }
+    if(e.key=="ArrowRight"){
+        count+= 50;
+        bird.style.left= count + "px" ;
+    }
+    if(e.key=="ArrowLeft"){
+        count-=  50;
+        bird.style.left= count + "px" ;
+    }
+    if(e.key=="ArrowUp"){
+        count+=  50;
+        bird.style.left= count + "px" ;
+    }
+
+
+
+
+
+    });
+
+
+
